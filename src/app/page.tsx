7f4ab1f3df7ad483f5578639ac78cadef552a3d3
RizDas <dkkrishabh@gmail.com>
2025-05-15 10:46:34 +0530

    Hero Component of Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
-import Image from "next/image";
+import React from "react";
+import about from "@styles/about.module.css";
+import home from "@styles/home.module.css";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="">
+    <div className={about.about}>
+      <div className={home.main}>
+        <h3 className={home.titlesub}>WELCOME TO</h3>
+        <h3 className={home.title}>THE TECH CLUB</h3>
+        <p className={home.description}>
+          Dedicated to learning more about the vast world of technology, and
+          helping others do the same with the help of our student mentors, and
+          participating in various tech-themed events.
+        </p>
+        <button className={home.button}>Apply Now</button>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
